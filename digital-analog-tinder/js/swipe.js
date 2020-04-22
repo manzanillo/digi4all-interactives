30b95aa55126c70ea4fac26a1cdddd5703bc2221
Stefan <manza@outlook.de>
2020-04-22 14:41:31 +0200

    upd digital-analog-tinder

--- digital-analog-tinder/js/swipe.js
+++ digital-analog-tinder/js/swipe.js
@@ -1,84 +1,92 @@
 var correct = 0;
 var incorrect = 0;
 
-$.urlParam = function(name){
-    var results = new RegExp('[\?&]' + name).exec(window.location.href);
-    if (results==null){
-       return false;
-    }
-    else{
-       return true;
-    }
-}
+$.urlParam = function (name) {
+  var results = new RegExp("[?&]" + name).exec(window.location.href);
+  if (results == null) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
-$(document).ready(function(){
+document.addEventListener("keydown", (event) => {
+  switch (event.which) {
+    case 37: // left
+      $('.card[style*="display: block;"]').trigger("swipeleft");
+      break;
+    case 39: // right
+      $('.card[style*="display: block;"]').trigger("swiperight");
 
-    var comments = $.urlParam('comments');
-    
-    $(".card").on("swiperight", function(){
-        if ($('.rotate-left:visible').length || $('.rotate-right:visible').length) {
-            return;
-        }
-        $(this).addClass('rotate-left').delay(500).fadeOut(2);
-        $('.card').find('.status').remove();
+      break;
 
-        if ($(this).attr('data-sol') == 'analog'){ //(solution[correct+incorrect] == 'analog'){
-            correct ++;
-            $('.speech-bubble').text($(this).attr('data-note-correct'))
-            $('#correct').html(correct);
-        }
-        else {
-            incorrect++;
-            $('.speech-bubble').text($(this).attr('data-note-wrong'));
-            $('#incorrect').html(incorrect);
-        }
-        
-        
-        if (comments) {
-            $('.speech-bubble-container').finish();
-            $('.speech-bubble-container').show().delay(6000).fadeOut();
-        }
-
-        $(this).append('<div class="status like">Analog!</div>');      
-        if ( $(this).is(':last-child') ) {
-            $('#container').html('<div class="message>Ende</div>')
-        } else {
-            $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-        }
-    });  
-
-
-    $(".card").on("swipeleft", function(){                
-        if ($('.rotate-left:visible').length || $('.rotate-right:visible').length) {
-            return;
-        }
-        $(this).addClass('rotate-right').delay(500).fadeOut(2);
-        $('.card').find('.status').remove();
-        $(this).append('<div class="status dislike">Digital!</div>');
-
-        if ($(this).attr('data-sol') == 'digital'){
-            correct ++;
-            $('.speech-bubble').text($(this).attr('data-note-correct'))
-            $('#correct').html(correct);
-        }
-        else {
-            incorrect++;
-            $('.speech-bubble').text($(this).attr('data-note-wrong'));
-            $('#incorrect').html(incorrect);
-        }
-
-        if (comments) {
-            $('.speech-bubble-container').finish();
-            $('.speech-bubble-container').show().delay(6000).fadeOut();
-        }
-            
-        if ( $(this).is(':last-child') ) {
-            $('#container').html('<div class="message">Ende</div>')
-         } else {
-            $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
-        } 
-   });
-
+    default:
+      return; // exit this handler for other keys
+  }
+  event.preventDefault(); // prevent the default action (scroll / move caret)
 });
 
+$(document).ready(function () {
+  var comments = $.urlParam("comments");
 
+  $(".card").on("swiperight", function () {
+    if ($(".rotate-left:visible").length || $(".rotate-right:visible").length) {
+      return;
+    }
+    $(this).addClass("rotate-left").delay(500).fadeOut(2);
+    $(".card").find(".status").remove();
+
+    if ($(this).attr("data-sol") == "analog") {
+      //(solution[correct+incorrect] == 'analog'){
+      correct++;
+      $(".speech-bubble").text($(this).attr("data-note-correct"));
+      $("#correct").html(correct);
+    } else {
+      incorrect++;
+      $(".speech-bubble").text($(this).attr("data-note-wrong"));
+      $("#incorrect").html(incorrect);
+    }
+
+    if (comments) {
+      $(".speech-bubble-container").finish();
+      $(".speech-bubble-container").show().delay(6000).fadeOut();
+    }
+
+    $(this).append('<div class="status like">Analog!</div>');
+    if ($(this).is(":last-child")) {
+      $("#container").html('<div class="message>Ende</div>');
+    } else {
+      $(this).next().removeClass("rotate-left rotate-right").fadeIn(400);
+    }
+  });
+
+  $(".card").on("swipeleft", function () {
+    if ($(".rotate-left:visible").length || $(".rotate-right:visible").length) {
+      return;
+    }
+    $(this).addClass("rotate-right").delay(500).fadeOut(2);
+    $(".card").find(".status").remove();
+    $(this).append('<div class="status dislike">Digital!</div>');
+
+    if ($(this).attr("data-sol") == "digital") {
+      correct++;
+      $(".speech-bubble").text($(this).attr("data-note-correct"));
+      $("#correct").html(correct);
+    } else {
+      incorrect++;
+      $(".speech-bubble").text($(this).attr("data-note-wrong"));
+      $("#incorrect").html(incorrect);
+    }
+
+    if (comments) {
+      $(".speech-bubble-container").finish();
+      $(".speech-bubble-container").show().delay(6000).fadeOut();
+    }
+
+    if ($(this).is(":last-child")) {
+      $("#container").html('<div class="message">Ende</div>');
+    } else {
+      $(this).next().removeClass("rotate-left rotate-right").fadeIn(400);
+    }
+  });
+});
